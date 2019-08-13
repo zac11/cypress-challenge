0668610151c9315b7052cf3741f50809023a889c
zac11 <ydv.rahul09@gmail.com>
2019-08-13 23:07:59 +0530

    Day 6 done- screenshots learnt

--- cypress/integration/challenge/day5_6.js
+++ cypress/integration/challenge/day5_6.js
@@ -22,3 +22,44 @@ describe('Random element selection',()=>{
         })
     })
 })
+
+// open test runner in a single device
+describe('Start tests with iphone resolution',()=>{
+    context('Header tests',()=>{
+        beforeEach(()=>{
+            cy.viewport('iphone-6');
+        });
+
+    it('displays header in mobile',()=>{
+        cy.get('.sf-menu').should('not.be.visible');
+        cy.get('.cat-title').click();
+        cy.get('.sf-menu').should('be.visible');
+    });
+
+})
+
+});
+
+//open test runner in multiple devices one by one
+describe('Open a page in different resolutions',()=>{
+
+    //this makes cypress take screenshot only of the viewport
+    Cypress.Screenshot.defaults({
+        capture: 'viewport'
+    });
+    const devices=['iphone-6+','ipad-2',[1280,800]];
+    devices.forEach((size)=>{
+        it(`should open the website in the ${size} resolution`,()=>{
+            if(Cypress._.isArray(size)){
+                cy.viewport(size[0],size[1])
+            }else{
+                cy.viewport(size)
+            }
+
+            cy.visit('http://automationpractice.com/index.php');
+            //screenshot will take full page screenshots if the size is not defined
+            //from above defaults command in line 47,48
+            cy.screenshot();
+        })
+    })
+})
